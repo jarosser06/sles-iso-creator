632ee4651b73015be4a3b7ce9300dd520e9f3963
Jim Rosser <jarosser06@gmail.com>
2014-04-03 20:38:33 -0500

    all the things

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
-VMWare Template
-===============
+SLES Custom ISO Builder
+=======================
 
-Using these generated cd's require that you have a web server
-serving the bootstrap files.
+Builds a custom iso for vmware template building b/c I am super lazy :).
 
-Random stuffs to build a custom Ubuntu iso inside a vagrant box.
+
+Download the SLES CDROM and rename it to sles.iso then:
 
 ```bash
-# Setup the env (downloads iso and runs vagrant up)
-./setup_env.sh
-
 vagrant ssh
 sudo su
 ./iso/build_iso.sh
 ```
-
-Currently only has seed file for installing Ubuntu with Chef 11.8.
 
 Vagrant
 -------
@@ -23,8 +18,3 @@ Vagrant
 Why does this require vagrant to work?  Using Vagrant to build the iso
 ensures a consistent environment to run the appropriate steps.  Also I
 run Fedora as my primary desktop sooo there is that too.
-
-TODO
-----
-* Would be nice if you could pass parameters to drop in bootstrap files
-* Add support for a network install
--- build_iso.sh
+++ build_iso.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
 VAGRANT_HOME=/home/vagrant
-BUILD_FILES=${VAGRANT_HOME}/iso/build_files
-SEED_LOCATION=${VAGRANT_HOME}/iso/seeds
-SEED_FILES="chef-bootstrap-seed.cfg chefserver-bootstrap-seed.cfg"
-NEW_ISO_NAME=ubuntu-1204-chef-`date +%F-%T`.iso
+NEW_ISO_NAME=SLES11-SP3-custom-x86_64-`date +%F`.iso
+BOOT_FILES="isolinux.cfg message"
 
 apt-get update
 apt-get install -y mkisofs
 
 echo "Mounting iso"
 mkdir -p /mnt/iso
-mount -o loop /home/vagrant/iso/UbuntuServer.iso /mnt/iso
+mount -o loop /home/vagrant/iso/sles.iso /mnt/iso
 
 echo "Copying original iso"
 mkdir -p /opt/serveriso
 cp -rT /mnt/iso /opt/serveriso
-chmod -R 777 /opt/serveriso/
+#chmod -R 777 /opt/serveriso/
 
 echo "Modifying original"
 pushd /opt/serveriso
-echo en > isolinux/langlist
 
-echo "Enter the host of your bootstrap files:"
-read bootstrap_server
-
-# copy all the seed files listed in seed files
-for SEED in $SEED_FILES
-do
-  cp ${SEED_LOCATION}/${SEED} /opt/serveriso/preseed/${SEED}
-  sed -i "s|BOOTSTRAP_SERVER|${bootstrap_server}|" /opt/serveriso/preseed/${SEED}
+# Add automation files
+cp ${VAGRANT_HOME}/iso/iso_files/autoinst.xml /opt/serveriso/
+for file in $BOOT_FILES; do
+  cp ${VAGRANT_HOME}/iso/iso_files/${file} /opt/serveriso/boot/x86_64/loader/${file}
 done
-cp ${BUILD_FILES}/txt.cfg /opt/serveriso/isolinux/txt.cfg
 
-chmod -R 777 /opt/serveriso/
-
-echo "Creating iso"
-mkisofs -D -r -V "ATTENDLESS_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o /home/vagrant/iso/${NEW_ISO_NAME} /opt/serveriso
+mkisofs -R -o ${VAGRANT_HOME}/iso/${NEW_ISO_NAME} -b boot/x86_64/loader/isolinux.bin \
+-c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table .
 echo "New iso ${NEW_ISO_NAME} created"
 
 echo "Cleaning up"
